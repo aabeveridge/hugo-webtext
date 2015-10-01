6393e3bc540155629fe822532ee4739d5617d4c8
aaronbev79 <aaronbev79@gmail.com>
2015-10-01 15:48:47 -0400

    updates

--- analysis.r
+++ analysis.r
@@ -9,12 +9,21 @@ d <- read.csv("~/hugo-webtext/ha-survey1.csv")
 # Survey Participant Summary
 ##############################
 
-# Map of participant countries
+#Graph of participant countries
+country.freq <- table(d$country)
+country.freq <- rev(country.freq)
+png("~/hugo-webtext/images/country-bar.png", 750, 800)
+  #Set margin sizes and window size for plot
+  par(mar=c(0, 20, 13, 4), pin=c(7, 8))
+  #Barplot
+  barplot(country.freq, las=2, main="Participant Countries", width=6, space=0.2, ylim=c(0, 250), horiz=TRUE, 
+          axes=FALSE)
+  #Add bottom axis and set numeric range
+	axis(1, at=seq(0, 110, 5))
+dev.off()
+
 
 # Graph of how participants became fans
 
-
-
-
-
+#############################
 
--- article.md
+++ article.md
@@ -1,3 +1,9 @@
 #Genre and Public Rhetoric: Social Justice and the Hugo Awards
 
 This article investigates the public discourse surrounding the 2015 Hugo Awards. 
+
+
+##
+![](./images/country-bar.png)
+
+
